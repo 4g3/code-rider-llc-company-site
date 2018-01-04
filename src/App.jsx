6ee22db15b72b71c69f0fa28815076360ea63c38
John Hillegass <hillegassdev@gmail.com>
2018-01-04 12:30:51 -0500

    update app.jsx to use router and link to about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
-function App() {
-  return (
-    <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-  );
-}
+const App = () =>
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={About} />
+      </Wrapper>
+      <Footer />
+    </div>
+  </Router>;
 
 export default App;
