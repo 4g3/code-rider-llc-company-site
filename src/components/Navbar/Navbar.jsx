6219747aa4f29ec832391a1145f0e9f3759ce3a0
John Hillegass <hillegassdev@gmail.com>
2018-01-04 12:29:50 -0500

    add footer and navbar components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Navbar = props =>
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <Link className="navbar-brand" to="/">
+          Pinder
+        </Link>
+      </div>
+      <ul className="nav navbar-nav">
+        <li
+          className={window.location.pathname === "/discover" ? "active" : ""}
+        >
+          <Link className="navbar-brand" to="/discover">Discover</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>;
+
+export default Navbar;
