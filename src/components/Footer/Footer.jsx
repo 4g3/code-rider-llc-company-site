97161dcd947ca5afa36acb4db148352e110b4f47
John Hillegass <hillegassdev@gmail.com>
2018-01-06 11:24:48 -0500

    update spacing on Footer and About components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () =>
-  (<footer className="footer">
-    <p>Made with <i className="fa fa-heart" aria-hidden="true" style={{ color: 'red' }} /> by <a href="/">Code Rider</a></p>
-   </footer>);
+  (
+    <footer className="footer">
+      <p>Made with <i className="fa fa-heart" aria-hidden="true" style={{ color: 'red' }} /> by <a href="/">Code Rider</a></p>
+    </footer>
+  );
 
 export default Footer;
