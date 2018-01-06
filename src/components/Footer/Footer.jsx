68c7f9759a3fc8703d7094a67a8070fc61f712de
John Hillegass <hillegassdev@gmail.com>
2018-01-06 11:18:54 -0500

    update footer a tag to link to project root

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () =>
   (<footer className="footer">
-    <p>Made with <i className="fa fa-heart" aria-hidden="true" style={{ color: 'red' }} /> by <a href="http://coderider.co">Code Rider</a></p>
+    <p>Made with <i className="fa fa-heart" aria-hidden="true" style={{ color: 'red' }} /> by <a href="/">Code Rider</a></p>
    </footer>);
 
 export default Footer;
