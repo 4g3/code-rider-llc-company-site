021c10dc86c1db2ad8ad57126d596c1633a7ab7f
John Hillegass <hillegassdev@gmail.com>
2018-03-22 00:26:09 -0400

    update hero component jsx and css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,10 +3,13 @@ import './Hero.css';
 // import logo from "../../assets/pug.png"
 
 const Hero = props =>
-  (<div
-    className="hero text-center"
-   >
-    {props.children}
-   </div>
+  (
+    <section className="hero is-info is-small">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          {props.children}
+        </div>
+      </div>
+    </section>
   );
 export default Hero;
