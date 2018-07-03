4833c07898908fb4a456be1a82b2744df3f05fca
John Hillegass <hillegassdev@gmail.com>
2018-07-03 01:06:39 -0400

    update portfolio page structure

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,61 +4,23 @@ class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      skillData: [
-        [
+      projectData: [
           {
-            src: 'salesforce.png',
-            alt: 'salesforce',
+          name: 'USDA',
           },
-          {
-            src: 'bootstrap.png',
-            alt: 'bootstrap',
-          },
-          {
-            src: 'sass.png',
-            alt: 'sass',
-          },
-        ],
-        [
-          {
-            src: 'jquery.png',
-            alt: 'jquery',
-          },
-          {
-            src: 'octocat.png',
-            alt: 'github',
-          },
-          {
-            src: 'vue-logo.png',
-            alt: 'Vue',
-          },
-        ],
       ],
     };
   }
   componentDidMount() { }
   render() {
-    const skillsList = this.state.skillData.map((arr, i) => (
-      <div className="columns is-mobile">
-        {arr.map((s, i) => (
-          <div>
-            <div className="column" >
-              Project
-            </div>
-            <div className="modal">
-              <div className="modal-background" />
-              <div className="modal-content">
-                Hello
-              </div>
-              <button className="modal-close is-large" aria-label="close" />
-            </div>
-          </div>
-        ))}
+    const projectsList = this.state.projectData.map((project, i) => (
+      <div className="notification">
+        Hello <strong>{project.name}</strong>
       </div>
     ));
     return (
-      <div className="container" style={{ marginTop: 30 }}>
-        {skillsList}
+      <div className="container is-fluid" style={{ marginTop: 60 }}>
+        {projectsList}
       </div>
     );
   }
