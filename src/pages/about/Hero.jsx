15e52588b4d54c8d242db8587d8dabf74265b593
John Hillegass <hillegassdev@gmail.com>
2018-07-04 20:42:50 -0400

    require children prop on Hero component

--- src/pages/about/Hero.jsx
+++ src/pages/about/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Hero = props => (
   <section className="hero is-info is-small">
@@ -9,4 +10,9 @@ const Hero = props => (
     </div>
   </section>
 );
+
+Hero.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
 export default Hero;
--- src/pages/main/Wrapper.jsx
+++ src/pages/main/Wrapper.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const wrap = {
   minHeight: '100%',
   paddingBottom: '100px',
+  marginTop: '50px',
 };
 
 const Wrapper = props => <main style={wrap} {...props} />;
