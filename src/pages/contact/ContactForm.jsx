130d4361e7642d5e6e09622159f4ff91aa51f915
John Hillegass <hillegassdev@gmail.com>
2018-07-11 20:22:01 -0400

    add contact form using formspree

--- src/pages/contact/ContactForm.jsx
+++ src/pages/contact/ContactForm.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 const ContactForm = () =>
   (
     <div>
+      <form method="POST" action="https://formspree.io/hillegassdev@gmail.com">
       <div className="field">
         <label className="label" htmlFor="name">Name</label>
         <div className="control">
-          <input className="input" type="text" placeholder="Your Name" />
+          <input className="input" type="text" placeholder="Your Name" name="visitor_name" />
+          <input type="text" name="_gotcha" style={{ display: 'none' }} />
         </div>
       </div>
 
       <div className="field">
         <label className="label" htmlFor="email">Email</label>
         <div className="control has-icons-left has-icons-right">
-          <input className="input" type="email" placeholder="Your Email Address" />
+          <input className="input" type="email" placeholder="Your Email Address" name="_replyto" />
           <span className="icon is-small is-left">
             <i className="fa fa-envelope-o" />
           </span>
@@ -24,7 +26,7 @@ const ContactForm = () =>
         <label className="label" htmlFor="subject">Subject</label>
         <div className="control">
           <div className="select">
-            <select>
+            <select name="subject">
               <option>Let's Chat</option>
               <option>Sales Inquiry</option>
               <option>Support Request</option>
@@ -38,7 +40,7 @@ const ContactForm = () =>
             Message
         </label>
         <div className="control">
-          <textarea className="textarea" placeholder="Description of the request" />
+          <textarea className="textarea" placeholder="Description of the request" name="message" />
         </div>
       </div>
       <div className="field is-grouped">
@@ -49,6 +51,7 @@ const ContactForm = () =>
           <button className="button is-text">Cancel</button>
         </div>
       </div>
+      </form>
     </div>
   );
 
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,27 @@
-export default {
+import axios from 'axios';
 
+export default {
+  sendEmail: () => {
+    axios({
+      method: 'post',
+      url: 'https://api.mailgun.net/v3/mailgunDomainName/messages',
+      auth: {
+        username: 'api',
+        password: 'apiKey',
+      },
+      params: {
+        from: 'Awesome Development Team <noreply@yourdomain.com>',
+        to: 'testing@example.com',
+        subject: 'Hello',
+        text: 'Welcome to the team!',
+      },
+    }).then(
+      (response) => {
+        console.log(response);
+      },
+      (reject) => {
+        console.log(reject);
+      },
+    );
+  },
 };
