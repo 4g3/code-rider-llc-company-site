792489d1dd1bb10fa52b04d49fc8d75694bfbf2c
John Hillegass <hillegassdev@gmail.com>
2020-02-25 11:19:32 -0500

    Updates

---
+++ precache-manifest.44bc3a50f8393f8756356c557df9d89d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b292ceba2c6d0b12b3eeadfcdc27769c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c4c24593493b99bb50b",
+    "url": "/static/css/main.9ca52b08.chunk.css"
+  },
+  {
+    "revision": "76463fa06b49487c2406",
+    "url": "/static/js/2.0b752f44.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0b752f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4c24593493b99bb50b",
+    "url": "/static/js/main.e28440c4.chunk.js"
+  },
+  {
+    "revision": "7f5b45c842b8eeb41dbe",
+    "url": "/static/js/runtime-main.86ead2aa.js"
+  }
+]);
