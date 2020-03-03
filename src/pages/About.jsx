18a8ecf0938a81ff9b9d98c744ecb88bc499b164
John Hillegass <hillegassdev@gmail.com>
2020-03-03 18:38:44 -0500

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,13 +12,11 @@ const About = () =>
         <div className="container is-fluid">
           <div className="columns is-mobile is-centered">
             <div className="column is-offset-one-half">
-              <h1>Let's ride the wave of technology together! </h1>
+              <h1>Forge (4g3) the best tooling and approach to cost, security and performance efficency in the cloud!</h1>
               <br />
               <p>
-              4g3 specializes in DevOps, SaaS and PaaS Application Development. 
               We have experience using Amazon Web Services (AWS), Google Cloud Platform (GCP), Heroku, ServiceNow, Workday and Salesforce.
-              We have worked with clients ranging from small Non-Profits to Goverment Entities and Fortune 100 clients.
-              Our team mainly specializes in Javascript (Node.js and React), Python, Ruby, Java and Apex development but also has experience developing native applications in iOS and Android.
+              We have worked with clients ranging from small non-profits to government entities and Fortune 100 clients.
               We would love to help you to implement your idea, drive digital transformation, manage your systems or solve your technology problems. 
               Please reach out on the {<Link to="/contact">Contact</Link>} page to start a discussion.
               </p>
